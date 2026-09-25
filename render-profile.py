07057ad834a6aeac0c86db51712ebe3caf6a696b
avalonreset <avalonreset@gmail.com>
2026-09-25 00:58:31 -0400

    Trim router description for single-line fit

--- render-profile.py
+++ render-profile.py
@@ -44,7 +44,7 @@ end=s.index('</table>',start)+len('</table>')
 s=s[:start]+'''<table width="100%"><tr>
 <td width="20%" valign="top"><img align="left" src="assets/column-width-240.svg" width="240" height="1" alt="" /><a href="https://github.com/avalonreset/cto-legends"><strong><code>cto-legends</code></strong></a></td>
 <td width="20%" valign="top"><img align="left" src="assets/column-width-240.svg" width="240" height="1" alt="" /><strong>single-skill-router</strong></td>
-<td width="60%" valign="top"><img align="left" src="assets/column-width-720.svg" width="720" height="1" alt="" />the central capability index, module manager, and execution router for legends.</td>
+<td width="60%" valign="top"><img align="left" src="assets/column-width-720.svg" width="720" height="1" alt="" />central capability index, module manager, and execution router for legends.</td>
 </tr></table>'''+s[end:]
 s=s.replace('height="56" alt=""','height="90" alt=""')
 old='<tr><th width="20%" align="left">Module</th><th width="20%" align="left">Focus</th><th width="60%" align="left">What you can do</th></tr>'
